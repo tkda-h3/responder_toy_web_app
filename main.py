606a0b759eff4f3639f57c25601339ef58bd67f4
tkda-h3 <18517117+tkda-h3@users.noreply.github.com>
2020-11-27 16:34:44 +0900

    first responder

---
+++ main.py
@@ -0,0 +1,18 @@
+import responder
+from responder import Request, Response
+
+api = responder.API()
+
+
+@api.route('/hello/{who}/html')
+def hello(req: Request, resp: Response, *, who: str):
+    # templatesディレクトリに配置
+    resp.html = api.template('hello.html', who=who)
+
+    # local()[f'HTTP_{number}'] = number の形で定数を作成しているので補完が効かない
+    print(api.status_codes.HTTP_200)
+
+
+if __name__ == '__main__':
+    api.run()
+
